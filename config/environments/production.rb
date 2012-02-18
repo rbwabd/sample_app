ebcedf74447878bcb262a033404a3e9f7e8a517a
rbwabd <rbwabd@yahoo.com>
2012-02-18 23:15:33 +0000

    production.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ SampleApp::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+	config.assets.precompile += %w( *.css *.js )
+
+	
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
